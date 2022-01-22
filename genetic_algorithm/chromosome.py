fec92e21503a61f66eec85bfcf4b6ae2fedad8c7
kdemirta <keremdemirtas@gmail.com>
2022-01-23 00:55:12 +0300

    more placeholders boilerplates and docstrings

---
+++ genetic_algorithm/chromosome.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class Chromosome:
+    """
+    Chromosome class. A chromosome represents an individual solution
+    within the solution space.
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        # User-friendly display of the chromosome
+        pass
+
+    def crossover(self, other):
+        """
+        Takes an other chromosome and performs a crossover with it.
+        Args:
+            other -> Chromosome instance
+        Returns:
+            children -> Numpy array of children containing two new chromosome instances
+        """
+        children = np.array([])
+
+        return children
+
+    def mutate(self):
+        """
+        Performs a mutation operation on the instance itself.
+        Args:
+
+        Returns -> Mutated chromosome instance
+        """
+        pass
---
+++ genetic_algorithm/utils/helpers.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def ensure_np_array(arr):
+    """
+    Takes arr as input and makes sure the array type is numpy.array. If it is not,
+    converts it to a numpy array.
+    Args:
+        arr -> An array-like object
+    Returns:
+        nparr -> Numpy array
+    """
+    nparr = None
+
+    return nparr
